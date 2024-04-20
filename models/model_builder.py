b6f12846dcbd060a5e6b0529f8d61ca1dbe44b3d
Ghost Popsicle <jasonsheinkopf@gmail.com>
2024-04-20 20:19:47 +0900

    removed qlang

--- models/model_builder.py
+++ models/model_builder.py
@@ -3,7 +3,7 @@ Different architectures of BLIP2 models.
 """
 
 from .build import MODEL_REGISTRY
-from .baseline_models import opt_2_7, blip2_quant, opt_2_7_qlang, opt_1_3, opt_2_7_identity
+from .baseline_models import opt_2_7, blip2_quant, opt_1_3, opt_2_7_identity
 
 # register function
 MODEL_REGISTRY.register(opt_2_7)
