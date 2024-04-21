0f75c404badf0625a7892dadbf2c99bd0dedf9d2
Ghost Popsicle <jasonsheinkopf@gmail.com>
2024-04-22 08:57:25 +0900

    modified base model params to be required or not

--- models/model_builder.py
+++ models/model_builder.py
@@ -3,11 +3,11 @@ Different architectures of BLIP2 models.
 """
 
 from .build import MODEL_REGISTRY
-from .baseline_models import opt_2_7, blip2_quant, opt_1_3, opt_2_7_identity, opt_1_3_qformer_projection
+from .baseline_models import opt_2_7, opt_2_7_no_peft, opt_1_3, opt_2_7_identity, opt_1_3_qformer_projection
 
 # register function
 MODEL_REGISTRY.register(opt_2_7)
-MODEL_REGISTRY.register(blip2_quant)
+MODEL_REGISTRY.register(opt_2_7_no_peft)
 MODEL_REGISTRY.register(opt_2_7_identity)
 MODEL_REGISTRY.register(opt_1_3)
 MODEL_REGISTRY.register(opt_1_3_qformer_projection)
