a6690051905b6f7dec24d14c33b1ffb8d9a5d6bf
Ghost Popsicle <jasonsheinkopf@gmail.com>
2024-04-23 12:00:25 +0900

    added feature optoni

--- data/nytimes.py
+++ data/nytimes.py
@@ -18,7 +18,7 @@ class ImageCaptioningDataset(Dataset):
         encoding = self.processor(images=item["image"], padding="max_length", return_tensors="pt")
         # remove batch dimension
         encoding = {k: v.squeeze() for k, v in encoding.items()}
-        encoding["text"] = item[self.feature]
+        encoding["text"] = item['questions']
         return encoding
 
 
